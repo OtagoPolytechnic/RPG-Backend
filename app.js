c95268cdfafc873a471d48a90ee1898eaaf7acb4
CalebStephens <calebtaylorstephens@gmail.com>
2023-08-03 18:19:35 +1200

    refactor(routing): fixed routing paths for builds and character

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ import rateLimit from 'express-rate-limit';
 
 import auth from './routes/v1/auth.js';
 import authRoute from './middleware/authRoute.js';
-import seedBuilds from './controllers/v1/seedBuilds.js';
-import { getAllBuilds } from './controllers/v1/builds.js';
+import character from './routes/v1/character.js';
+import builds from './routes/v1/builds.js';
 
 dotenv.config();
 const app = express();
@@ -53,9 +53,8 @@ const getAvailableEndpoints = () => {
   return data;
 };
 
-app.use(`/${BASE_URL}/${CURRENT_VERSION}/admin/seed`, seedBuilds);
-app.use(`/${BASE_URL}/${CURRENT_VERSION}/builds`, getAllBuilds);
-
+app.use(`/${BASE_URL}/${CURRENT_VERSION}/builds`,authRoute, builds);
+app.use(`/${BASE_URL}/${CURRENT_VERSION}/character`, authRoute, character);
 app.use(`/${BASE_URL}/${CURRENT_VERSION}/auth`, auth);
 app.get(`/${BASE_URL}/${CURRENT_VERSION}`, (req, res) =>
   res.status(200).json(getAvailableEndpoints())
