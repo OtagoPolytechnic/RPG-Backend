507eca7cc82a78e772662d9e8f1b55695d6f48f2
CalebStephens <calebtaylorstephens@gmail.com>
2023-08-03 18:20:30 +1200

    fix(gist): updated gist uri

--- controllers/v1/seedBuilds.js
+++ controllers/v1/seedBuilds.js
@@ -6,16 +6,13 @@ const prisma = new PrismaClient();
 const seedBuilds = async (req, res) => {
   try{
     const data = await axios.get(
-        'https://gist.githubusercontent.com/CalebStephens/87f26088808256115b04c0b8918b6412/raw/d6326f3c471bb16c884045406b2a851056569106/rpgRoles.json'
+        'https://gist.githubusercontent.com/CalebStephens/87f26088808256115b04c0b8918b6412/raw/b0061455b5c0c9681e2eb51f813f91884dee6ea0/rpgRoles.json'
       );
       await prisma.build.createMany({ data: data.data });
       return res.status(200).json({data: data.data})
   }catch(error){
     return res.status(500).json({error: error.message});
   }
-
-    
-
 }
 
 export default seedBuilds;
