6088e77922a94032ba71957065dfc3c3d92d4874
Sasnz <r.koks@hotmail.co.nz>
2023-08-10 10:53:39 +1200

    started tests

--- cypress/e2e/api-users.cy.js
+++ cypress/e2e/api-users.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Auth testing', () => {
+  it("Admin user Login and get token", () => {
+    cy.request({
+        method: "POST",
+        url: "http://localhost:3001/api/v1/auth/login",
+        body: {
+            email: "Testman@gmail.com",
+            password: "P@ssw0rd"
+        },
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("token");
+        token = response.body.token;
+    });
+})
 })
