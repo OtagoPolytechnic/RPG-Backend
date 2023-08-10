ac32f8741419997baa001c3d106fa25dacb36a09
Sasnz <r.koks@hotmail.co.nz>
2023-08-10 13:25:36 +1200

    added Register user, second user, check user and delete user

--- cypress/e2e/api-users.cy.js
+++ cypress/e2e/api-users.cy.js
@@ -1,16 +1,75 @@
+const { expect } = require("chai")
+
 describe('Auth testing', () => {
-  it("Admin user Login and get token", () => {
-    cy.request({
-        method: "POST",
-        url: "http://localhost:3001/api/v1/auth/login",
-        body: {
-            email: "Testman@gmail.com",
-            password: "P@ssw0rd"
-        },
-    }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property("token");
-        token = response.body.token;
-    });
-})
+    let userId;
+    let token;
+
+    // Registers a new user
+    it("Registers a user", () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/v1/auth/register',
+            body: {
+                name: 'Freddy Testman',
+                email: 'FTest@test.com',
+                password: 'Ft3st123'
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body).to.have.property('msg');
+            expect(response.body.msg).to.eq('User successfully registered');
+        })
+    })
+    // END OF Registers a new user
+
+    // Registers a second user to database
+    it("Registers another user", () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/v1/auth/register',
+            body: {
+                name: 'Beetle Juice',
+                email: 'BeetleJ@test.com',
+                password: 'BeetlejuiceX3'
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body).to.have.property('msg');
+            expect(response.body.msg).to.eq('User successfully registered');
+        })
+    })
+    // END OF Registers a second user to database
+
+    // Checks that user already exists
+    it("Checks that user already exists", () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3001/api/v1/auth/register',
+            body: {
+                name: 'Freddy Testman',
+                email: 'FTest@test.com',
+                password: 'Ft3st123'
+            },
+            failOnStatusCode: false,
+        }).then((response) => {
+            expect(response.status).to.eq(409);
+            expect(response.body).to.have.property("msg", "User already exists");
+        })
+    })
+    // END OF Checks that user already exists
+
+    // Deletes a user
+    it("Deletes a user", () => {
+
+        cy.request({
+            method: "DELETE",
+            url: `http://localhost:3001/api/v1/users/${userId}`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    })
+    // END OF Deletes a user
 })
