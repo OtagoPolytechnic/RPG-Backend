1afe79f0e6696d2a2834abf620c9a6e840e66976
Sasnz <r.koks@hotmail.co.nz>
2023-08-17 13:42:59 +1200

    testing

---
+++ cypress/e2e/api-users-create.cy.js
@@ -0,0 +1,38 @@
+describe("user create", () => {
+    let userId;
+    let token;
+  
+    // Registers a new user
+    it("Registers a user", () => {
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3001/api/v1/auth/register",
+        body: {
+          username: "Freddy Testman",
+          password: "Ft3st123",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body).to.have.property("msg");
+        expect(response.body.msg).to.eq("User successfully registered");
+      });
+    }); // END OF Registers a new user
+  
+    // Registers a second user to database
+    it("Registers another user", () => {
+      cy.request({
+        method: "POST",
+        url: "http://localhost:3001/api/v1/auth/register",
+        body: {
+          username: "Beetle Juice",
+          password: "BeetlejuiceX3",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body).to.have.property("msg");
+        expect(response.body.msg).to.eq("User successfully registered");
+      });
+    });
+    // END OF Registers a second user to database
+  });
+  
