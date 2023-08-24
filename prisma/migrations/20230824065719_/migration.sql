cd1d00c8cef6691764d8f49ab503c6978b830a35
CalebStephens <calebtaylorstephens@gmail.com>
2023-08-24 19:02:00 +1200

    migration

---
+++ prisma/migrations/20230824065719_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `itemCharacterId` on the `Character` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Character" DROP COLUMN "itemCharacterId";
