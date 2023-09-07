32aaea60588685c52658aa7bea9877996dbea2ed
MarcoKoen <neokocram@gmail.com>
2023-09-07 14:22:18 +1200

    updated location

--- controllers/v1/location.js
+++ controllers/v1/location.js
@@ -2,30 +2,34 @@
  * Handles all location crelated endpoints
  */
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getLocations = async (req, res) => {
-    try{
-        const locations = await prisma.location.findMany();
-        return res.status(200).json({data: locations});
-        
-    }catch(e){
-        return res.status(500).json({error: error.message});
-    }
+  try {
+    const locations = await prisma.location.findMany();
+    return res.status(200).json({ data: locations });
+  } catch (e) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const getLocation = async (req, res) => {
-    try{
-        const locationId = req.params.id;
-        
-        const location = await prisma.location.findUnique({where: {id: Number(locationId)}});
-        console.log(location);
-        return res.status(200).json({data: location});
+  try {
+    const locationId = req.params.id;
 
-    }catch(e){
-        return res.status(500).json({error: e.message});
-    }
+    const location = await prisma.location.findUnique({
+      where: { id: Number(locationId) },
+    });
+
+    const items = await prisma.item.findMany({
+      where: { id: { in: location.items } },
+    });
+
+    return res.status(200).json({ data: location, items: items });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
 };
 
-export {getLocations, getLocation}
+export { getLocations, getLocation };
--- data/location.js
+++ data/location.js
@@ -1,12 +1,11 @@
 const locations = [
     {
       name: "Tutorial Island.",
-      description: "On this island there is a chest with a level 1 weapon.",
       items: [1]
     },
     {
       name: "Starter Island.",
-      description: "On this island there is a person to talk to."
+      items: [2,3]
     }
   ];
   
