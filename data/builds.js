9eefae66b3d3dac980b29e8a0e8d139f0c0f0389
CalebStephens <calebtaylorstephens@gmail.com>
2023-09-11 13:29:23 +1200

    change to json

--- data/builds.js
+++ data/builds.js
@@ -1,7 +1,7 @@
 const builds = [{
-    "title": "Fighter",
-    "description": "The Fighter class is a skilled and fearless warrior, specializing in close combat and protecting allies with their strength and martial abilities.",
-    "stats" : {
+    title: "Fighter",
+    description: "The Fighter class is a skilled and fearless warrior, specializing in close combat and protecting allies with their strength and martial abilities.",
+    stats : {
         "health": 6,
         "attack": 6,
         "defense": 4,
@@ -10,25 +10,25 @@ const builds = [{
     }
 },
 {
-    "title": "Ranged",
-    "description": "The Ranger class is a skilled archer and tracker, adept at ranged combat and nature-based abilities.",
-    "stats" : {
-        "health": 4,
-        "attack": 5,
-        "defense": 3,
-        "mana": 2,
-        "agility": 6
+    title: "Ranged",
+    description: "The Ranger class is a skilled archer and tracker, adept at ranged combat and nature-based abilities.",
+    stats : {
+        health: 4,
+        attack: 5,
+        defense: 3,
+        mana: 2,
+        agility: 6
     }
 },
 {
-    "title": "Mage",
-    "description": "The Mage is a spellcasting character specializing in arcane magic, wielding a diverse range of spells for offense, defense, and utility, while relying on their profound magical knowledge and often foregoing physical prowess.",
-    "stats" : {
-        "health": 3,
-        "attack": 5,
-        "defense": 2,
-        "mana": 6,
-        "agility": 4
+    title: "Mage",
+    description: "The Mage is a spellcasting character specializing in arcane magic, wielding a diverse range of spells for offense, defense, and utility, while relying on their profound magical knowledge and often foregoing physical prowess.",
+    stats : {
+        health: 3,
+        attack: 5,
+        defense: 2,
+        mana: 6,
+        agility: 4
     }
 }
 ]
