fcf1d28e974b428c4fe6f0a2b0622d5f596d08e3
CalebStephens <calebtaylorstephens@gmail.com>
2023-09-19 17:28:19 +1200

    added inline comments

--- controllers/v1/character.mjs
+++ controllers/v1/character.mjs
@@ -116,107 +116,124 @@ const characterItems = async (req, res) => {
   }
 };
 
-// Add an item to a character
 const addItemToCharacter = async (req, res) => {
-  try {
-    const { id } = req.user;
-    const user = await prisma.user.findUnique({
-      where: { id: Number(id) },
-      include: {
-        characters: true,
-      },
-    });
-
-    //check if the character belongs to the user
-    const characterAvailable = user.characters.find((character) => character.id === Number(req.body.characterId));
-    if (!characterAvailable) {
-      return res.status(401).json({ error: "You are not authorized to view this character" });
+    try {
+      const { id } = req.user;
+      // Fetch the user from the database based on the user's ID.
+      const user = await prisma.user.findUnique({
+        where: { id: Number(id) },
+        include: {
+          characters: true,
+        },
+      });
+  
+      // Check if the character belongs to the user.
+      const characterAvailable = user.characters.find((character) => character.id === Number(req.body.characterId));
+      if (!characterAvailable) {
+        return res.status(401).json({ error: "You are not authorized to view this character" });
+      }
+  
+      // Fetch the character from the database based on the character ID.
+      const character = await prisma.character.findUnique({
+        where: {
+          id: Number(req.body.characterId),
+        },
+      });
+  
+      // Fetch the item from the database based on the item ID.
+      const item = await prisma.item.findUnique({
+        where: {
+          id: Number(req.body.itemId),
+        },
+      });
+  
+      // Check if the character has enough currency to purchase the item.
+      if (character.currency < item.cost) {
+        return res.status(400).json({ error: "Insufficient funds" });
+      }
+  
+      // Update the character's currency after purchasing the item.
+      const updatedCharacter = await prisma.character.update({
+        where: {
+          id: Number(req.body.characterId),
+        },
+        data: {
+          currency: character.currency - item.cost,
+        },
+      });
+  
+      // Create a record of the item being added to the character's inventory.
+      const data = await prisma.itemCharacter.create({
+        data: {
+          characterId: Number(req.body.characterId),
+          itemId: Number(req.body.itemId),
+        },
+      });
+  
+      // Return a success response with relevant data.
+      return res.status(200).json({ data: data, updatedCharacter: updatedCharacter, message: "Item added" });
+    } catch (error) {
+      // Handle any errors that occur during the process.
+      return res.status(500).json({ error: error.message });
     }
-
-    const character = await prisma.character.findUnique({
-      where: {
-        id: Number(req.body.characterId),
-      },
-    });
-
-    const item = await prisma.item.findUnique({
-      where: {
-        id: Number(req.body.itemId),
-      },
-    });
-
-    if (character.currency < item.cost) {
-      return res.status(400).json({ error: "Insufficient funds" });
-    }
-
-    const updatedCharacter = await prisma.character.update({
-      where: {
-        id: Number(req.body.characterId),
-      },
-      data: {
-        currency: character.currency - item.cost,
-      },
-    });
-
-    const data = await prisma.itemChraracter.create({
-      data: {
-        characterId: Number(req.body.characterId),
-        itemId: Number(req.body.itemId),
-      },
-    });
-    return res.status(200).json({ data: data, updatedCharacter: updatedCharacter, message: "Item added" });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
+  };
+  
 
 const updateCharacter = async (req, res) => {
-  try {
-    const { id } = req.user;
-    const user = await prisma.user.findUnique({
-      where: { id: Number(id) },
-      include: {
-        characters: true,
-      },
-    });
-
-    //check if the character belongs to the user
-    const characterAvailable = user.characters.find((character) => character.id === Number(req.params.characterId));
-    if (!characterAvailable) {
-      return res.status(401).json({ error: "You are not authorized to view this character" });
+    try {
+      const { id } = req.user;
+      // Fetch the user from the database based on the user's ID.
+      const user = await prisma.user.findUnique({
+        where: { id: Number(id) },
+        include: {
+          characters: true,
+        },
+      });
+  
+      // Check if the character belongs to the user.
+      const characterAvailable = user.characters.find((character) => character.id === Number(req.params.characterId));
+      if (!characterAvailable) {
+        return res.status(401).json({ error: "You are not authorized to view this character" });
+      }
+  
+      // Fetch the character from the database based on the character ID.
+      const character = await prisma.character.findUnique({
+        where: {
+          id: Number(req.params.characterId),
+        },
+      });
+  
+      if (!character) {
+        return res.status(404).json({ error: "Character not found" });
+      }
+  
+      // Destructure the request body into a data object.
+      const { ...data } = req.body;
+  
+      // Define an array of fields that should not be updated.
+      const noUpdate = ["id", "userId", "buildId", "gender", "name"];
+      // Check if any of the disallowed fields are present in the update data.
+      if (noUpdate.some((key) => Object.keys(data).includes(key))) {
+        return res.status(400).json({ error: "A field you are trying to update is not allowed" });
+      }
+  
+      // Update the character in the database.
+      const updatedCharacter = await prisma.character.update({
+        where: {
+          id: Number(req.params.characterId),
+        },
+        data,
+      });
+  
+      return res.json({
+        msg: `${character.name} has been successfully updated`,
+        data: updatedCharacter,
+      });
+    } catch (error) {
+      // Handle any errors that occur during the process.
+      return res.status(500).json({ error: error.message });
     }
-
-    const character = await prisma.character.findUnique({
-      where: {
-        id: Number(req.params.characterId),
-      },
-    });
-
-    if (!character) {
-      return res.status(404).json({ error: "Character not found" });
-    }
-
-    const { ...data } = req.body;
-
-    const noUpdate = ["id", "userId", "buildId", "gender", "name"];
-    if (noUpdate.some((key) => Object.keys(data).includes(key))) {
-      return res.status(400).json({ error: "A field you are trying to update is not allowed" });
-    }
-
-    const updatedCharacter = await prisma.character.update({
-      where: {
-        id: Number(req.params.characterId),
-      },
-      data,
-    });
-
-    return res.json({
-      msg: `${character.name} has been successfully updated`,
-      data: updatedCharacter,
-    });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
+  };
+  
 
 export { createCharacter, getAllCharacters, getCharacter, characterItems, addItemToCharacter, updateCharacter };
