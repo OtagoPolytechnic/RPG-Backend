478c7234b8cb72c8c280b746190036db5e31c7df
MarcoKoen <neokocram@gmail.com>
2023-09-21 12:39:05 +1200

    updated location schema

--- data/location.js
+++ data/location.js
@@ -2,11 +2,15 @@ const locations = [
     {
       name: "Tutorial Island.",
       description: "On this island there is a chest with a level 1 weapon.",
+      level: 0,
       items: [1]
     },
     {
       name: "Starter Island.",
-      description: "On this island there is a person to talk to."
+      description: "On this island there is a person to talk to.",
+      level: 1,
+      items: [4]
+
     }
   ];
   
