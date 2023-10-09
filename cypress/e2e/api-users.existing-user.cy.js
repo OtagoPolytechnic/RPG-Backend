c4519d5058f2246dfcd2cba92664c5b3a9319ed7
Sasnz <r.koks@hotmail.co.nz>
2023-10-09 14:04:45 +1300

    minor tweaks and updates

--- cypress/e2e/api-users.existing-user.cy.js
+++ cypress/e2e/api-users.existing-user.cy.js
@@ -2,14 +2,13 @@ describe("existing users", () => {
   let userId;
   let token;
 
-  // Checks that user "Freddy Testman" already exists
-  it("Checks that user 'Freddy Testman' already exists", () => {
+  // Checks that user "Beetle Juice" already exists
+  it("Checks that user 'Beetle Juice' already exists", () => {
     cy.request({
       method: "GET",
       url: "http://localhost:3001/api/v1/auth/username",
       body: {
-        username: "Freddy Testman",
-        password: "Ft3st123",
+        username: "Beetle Juice",
       },
       failOnStatusCode: false,
     }).then((response) => {
@@ -17,4 +16,5 @@ describe("existing users", () => {
       expect(response.body).to.have.property("msg", "User already exists");
     });
   });
+  // END OF Checks that user "Beetle Juice" already exists
 });
