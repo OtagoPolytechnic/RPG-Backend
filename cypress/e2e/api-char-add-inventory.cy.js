380d077e6147dba425873c92641d1d6a6fa65f74
Devon Partridge-Officer <90590089+DevonPartridgeOfficer@users.noreply.github.com>
2023-11-01 08:46:41 +1300

    ADD: Tests for adding correct/incorrect items to character inventory

---
+++ cypress/e2e/api-char-add-inventory.cy.js
@@ -0,0 +1,88 @@
+//Test Item 4.1.06 /character/update:characterId
+//Description: Logs in as a user, creates a fresh character and add a correct/incorrect item to the inventory
+
+//Required Variables for character auth + updates
+let token; //Used to pass a token between login and checking character name 
+let charId;
+
+describe("Log in user", () => {
+    it("Admin user Login and get token", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3001/api/v1/auth/login",
+            body: {
+                username: "Freddy Testman",
+                password: "Ft3st123",
+            },
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property("token");
+            token = response.body.token;
+        });
+    });
+});
+
+describe("Create character", () => {
+    it("Create unique character name for future endpoint test", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3001/api/v1/character/create",
+            body: {
+                "name": "Tom Hanks",
+                "gender": "MALE",
+                "buildId": 2
+            },
+            headers: {
+                Authorization: `Bearer ${token}` // Set the Authorization header with the token
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property("data");
+            expect(response.body.data).to.have.property("id");
+            charId = response.body.data.id;
+        });
+    });
+});
+
+describe("Add item to inventory", () => {
+    it("Updates the character and adds a new item (with known characterId)", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3001/api/v1/character/inventory/add",
+            body: {
+                "characterId": charId,
+                "itemId": 1
+            },
+            headers: {
+                Authorization: `Bearer ${token}` // Set the Authorization header with the token
+            }
+        }).then((response) => {
+            console.log(response)
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property("data");
+            expect(response.body.message).to.eq("Item added");
+        });
+    });
+});
+
+describe("Add incorrect item to inventory", () => {
+    it("Updates the character and tries to add a new item (with incorrect itemId)", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3001/api/v1/character/inventory/add",
+            body: {
+                "characterId": charId,
+                "itemId": 1000
+            },
+            failOnStatusCode: false,
+            headers: {
+                Authorization: `Bearer ${token}` // Set the Authorization header with the token
+            }
+        }).then((response) => {
+            console.log(response)
+            expect(response.status).to.eq(404);
+            expect(response.body).to.have.property("error");
+            expect(response.body.error).to.eq("Item not found");
+        });
+    });
+});
