361e3dae48bed601cf37d50d8788ff1fab48aed2
Devon Partridge-Officer <90590089+DevonPartridgeOfficer@users.noreply.github.com>
2023-11-15 13:06:43 +1300

    ADD: Invalid Data type test for items - All integer values sent

--- cypress/e2e/Data Types/api-check-item-data.cy.js
+++ cypress/e2e/Data Types/api-check-item-data.cy.js
@@ -48,7 +48,7 @@ describe("Check API data types", () => {
     });
 });
 
-describe("Invalid item data - INT", () => {
+describe("Invalid item data - All Strings", () => {
     it("Trys adding new item with incorrect data for integers (string) and checks for status response code", () => {
         cy.request({
             method: "POST",
@@ -95,6 +95,64 @@ describe("Invalid item data - INT", () => {
                         expect(requestBody.buyCost).to.be.a("string");
                         expect(requestBody.sellPrice).to.be.a("string");
                         //console.log(response)
+                    } 
+                    else {
+                        cy.log("Request body is undefined for status 500");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        });
+    });
+});
+
+describe("Invalid item data - All Integers", () => {
+    it("Trys adding new item with incorrect data for strings (integers) and checks for status response code", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3001/api/v1/items/create",
+            body: {
+                "name": 100,
+                "type": 200,
+                "level": 5,
+                "rarity": 100,
+                "buyCost": 2,
+                "sellPrice": 1
+            },
+            failOnStatusCode: false,
+            headers: {
+                Authorization: `Bearer ${token}` // Set the Authorization header with the token
+            }
+        }).then((response) => {
+            //Create a JSON object for response unsuccessful checking
+            let requestBody;
+            if (typeof response.requestBody === 'string') {
+                requestBody = JSON.parse(response.requestBody);
+            } 
+            else {
+                requestBody = response.requestBody;
+            }
+
+            //Check if the API has done any internal error checking to change data types
+            switch (response.status){
+
+                case 200://Check to see if non-strings convert in API
+                    expect(response.status).to.eq(200);
+                    expect(response.requestBody.name).to.eq("100");
+                    expect(response.requestBody.name).to.be.a("string");
+                    expect(response.requestBody.type).to.be.a("string"); 
+                    expect(response.requestBody.rarity).to.be.a("string");
+                    break;
+
+                case 500://Check to see if stayed as int
+                    expect(response.status).to.eq(500);
+                    if (response.requestBody) {
+                        expect(requestBody.name).to.eq(100);
+                        expect(requestBody.name).to.be.a("number"); 
+                        expect(requestBody.type).to.be.a("number");
+                        expect(requestBody.rarity).to.be.a("number");
                     } 
                     else {
                         cy.log("Request body is undefined for status 500");
